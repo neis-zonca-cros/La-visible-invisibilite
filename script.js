019ef6c4488f66414a9e736d1b21691fbd0552d1
z0opreme <remy.engassac@icloud.com>
2023-11-21 10:57:00 +0100

    fetch all street works

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 const overpassQuery = `
-  [out:json];
-  area["name"="Lyon"]->.searchArea;
-  ( 
-      way["name"]["highway"~"residential|tertiary|primary|secondary|unclassified|road|living_street"](area.searchArea);
-  );
-  out body;
+[out:json][timeout:2500];
+area(id:3600120965)->.searchArea;
+(
+  way["highway"]["name"](area.searchArea);
+);
+for (t["name"])
+{
+  make street name=_.val;
+  out;
+}
 `;
 
-const overpassApiUrl = 'https://overpass-api.de/api/interpreter';
-
-fetch(overpassApiUrl, {
+fetch("https://overpass-api.de/api/interpreter", {
 	method: 'POST',
-	headers: {
-		'Content-Type': 'application/x-www-form-urlencoded',
-	},
-	body: `data=${encodeURIComponent(overpassQuery)}`,
+	mode: 'cors',
+	body: "data=" + encodeURIComponent(overpassQuery)
 })
-	.then(response => response.json())
-	.then(data => {
+	.then((response) => {
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		return response.json();
+	})
+	.then((data) => {
+		console.log(data);
 		const uniqueNames = Array.from(new Set(data.elements.map(element => element.tags.name))).filter(Boolean);
 		console.log(uniqueNames);
 	})
 	.catch(error => {
-		console.error(error);
+		console.error('There was a problem with the fetch operation:', error);
 	});
