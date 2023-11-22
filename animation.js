6d7e1cbc6ccca269b317370d35d34c72d228b94b
Néïs ZONCA-CROS <neis@MacBook-Pro-de-Neis.local>
2023-11-22 15:56:55 +0100

    carré 3

--- animation.js
+++ animation.js
@@ -1,11 +1,11 @@
-
-
 for (let i=0; i<10 ; i++){
     pos_x = Math.round(Math.random()*1024);
     pos_y = Math.round(Math.random()*768);
+    //créer carré (html)
+
 
     document.getElementById("carre").style.left = pos_x + "px";
-    document.getElementById("carre").style.top= pos_y + "py"; 
+    document.getElementById("carre").style.top= pos_y + "px"; 
 }
 
 
