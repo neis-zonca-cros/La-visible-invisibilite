509c7098251732c5b7dd5867105b7ad6e31dad27
Néïs ZONCA-CROS <neis@MacBook-Pro-de-Neis.local>
2023-11-23 10:07:02 +0100

    2215 carré

--- animation.js
+++ animation.js
@@ -1,11 +1,17 @@
-for (let i=0; i<10 ; i++){
-    pos_x = Math.round(Math.random()*1024);
+
+//création de 2216 carré en slide 2
+for (let i = 0; i<2215; i++){
+    var carre = document.createElement('div');
+    carre.id = "carre" + i;
+    carre.dataset.aos="zoom-in";
+    carre.dataset.aos.duration="3000";
+    carre.classList.add("carre");
+    document.querySelector(".slide.two").appendChild(carre);
+
+    pos_x = Math.round(Math.random()*1512);
     pos_y = Math.round(Math.random()*768);
-    //créer carré (html)
 
-
-    document.getElementById("carre").style.left = pos_x + "px";
-    document.getElementById("carre").style.top= pos_y + "px"; 
+    document.getElementById("carre" + i).style.left = pos_x + "px";
+    document.getElementById("carre" + i).style.top= pos_y + "px"; 
 }
-
 
